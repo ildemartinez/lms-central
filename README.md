ddb113413728ea8bb3186e55603daa3e23fe368c
Ildefonso Martínez <i.martinezmarchena@gmail.com>
2023-05-22 13:10:38 +0200

    Added swagger support to backend

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # lms-central
+
+## Install
+
+npm install into backend and frontend directories
+
+## Run
+
+Backend: npm run start:dev
+http://localhost:3000/
+
+API Documentation
+http://localhost:3000/doc
+
+Frontend: npm run start
+http://localhost:4200/
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API documentation')
+    .setDescription('The API documentation')
+    .setVersion('1.0')
+    //.addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
